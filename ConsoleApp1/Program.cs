47cdb11c016d5b1478b65a33c4b8f0df6a7cbce2
Haynik <haynik12467@icloud.com>
2019-12-23 09:55:06 -0500

    reupped

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,12 +6,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodbye World :o");
-            Console.WriteLine("Whose man is this?");
-            string response = Console.ReadLine();
-            Console.WriteLine("Well you said " + response + " and you're a bitch for not checking him");
+         
 
+            Console.WriteLine("'The Hallpass'");
+
+
+            Console.WriteLine("whats your name?");
+                string name = Console.ReadLine();
+
+            Console.WriteLine("and where do you wanna go?");
+
+            string place = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(name);
+            Console.WriteLine("is too cool for this stupid math class mr king.");
+            Console.WriteLine(" soooo he's dipping outta class and heading to ");
+            Console.WriteLine(place);
+            Console.WriteLine("because y'all are reaching");
+
+
+            Console.WriteLine(" and " + name +  " is teaching.");
+
+
+            
         } 
 
     }
